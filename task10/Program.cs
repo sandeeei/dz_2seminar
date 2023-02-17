4321d00f195e17c3bf8e877eaae48601a2850dfe
Sergei <sandeeey@example.com>
2023-02-17 10:06:39 +0300

    task_10

---
+++ task10/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход 
+// трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+ Console.Write("Введите трёхзначное число: ");
+ int number = Convert.ToInt32(Console.ReadLine());
+
+// if (number>999 || number<100)
+// {
+//     Console.Write($"Число:  {number} не является трёхзначным числом ");
+//     return;
+// }
+
+// int result = number%100/10;
+
+// Console.WriteLine($"Вторая цифра в числе {number} :  {result}");
+
+while(number>999 || number<100)
+{
+    Console.Write($"Число:  {number} не является трёхзначным числом, Введите ТРЁХЗНАЧНОЕ число:   ");
+    number = Convert.ToInt32(Console.ReadLine());
+}
+int result = number%100/10;
+
+Console.WriteLine($"Вторая цифра в числе {number} :  {result}");
+
+// работает))
